ced21885579adb7df83af00b41cbef5d58f5afd2
Josh Woodward <jdw6359@rit.edu>
2013-06-02 14:38:05 -0400

    Initial Commit of project resources

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e365b062eb91121b4209e007673f77f2e1b5139b23958d7de21a11ae616f72d593d51a83965b309862ad897c1c43d5c6a7a22a26c6b958423665f9d957e127c5'
